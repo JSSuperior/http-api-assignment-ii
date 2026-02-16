49d871a20fa0c211e98d1ff983aaee1f4d5ce0f2
JSSuperior <theoruefli@gmail.com>
2026-02-15 19:31:57 -0500

    Basic Server Set Up

---
+++ src/htmlResponses.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const client = fs.readFileSync(`${__dirname}/../client/client.html`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+const getClient = (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(client);
+    response.end();
+};
+
+const getCss = (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/css' });
+    response.write(css);
+    response.end();
+};
+
+module.exports = {
+    getClient,
+    getCss
+};
---
+++ src/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+    '/' : htmlHandler.getClient,
+    '/style.css' : htmlHandler.getCss,
+};
+
+const onRequest = (request, response) => {
+    const protocol = request.connection.encrypted ? 'https' : 'http';
+    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+
+    if(urlStruct[parsedUrl.pathname])
+    {
+        return urlStruct[parsedUrl.pathname](request, response);
+    }
+
+};
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Listening on 127.0.0.1${port}`);
+});
